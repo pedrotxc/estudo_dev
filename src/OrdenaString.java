43f00547f870c31fc2706764abb7b86f1422c41f
Pedro Henrique <pedrotxc@gmail.com>
2023-06-18 11:52:48 -0300

    projeto atualizado

--- src/OrdenaString.java
+++ src/OrdenaString.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class OrdenaString {
 	public static void main(String[] args) {
@@ -11,23 +12,38 @@ public class OrdenaString {
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
 		
-//		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));		
+//		ComparaString string = new ComparaString();
+		
+//		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+//		palavras.sort(Comparator.comparing(palavra -> palavra.length()));
 		palavras.sort(Comparator.comparing(String::length));
-		palavras.forEach(System.out::println);
+		System.out.println(palavras);
+		
+//		ConsomeString stringFor = new ConsomeString();
+//		palavras.forEach(stringFor);
+		
+//		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));		
+//		palavras.sort(Comparator.comparing(String::length));
+//		palavras.forEach(System.out::println);
 	}
 }
 
-class ComparaString implements Comparator<String>{
+//class ComparaString implements Comparator<String>{
+//	@Override
+//	public int compare(String o1, String o2) {
+//		if(o1.length() > o2.length()) {
+//			return 1;
+//		}
+//		if(o1.length() < o2.length()) {
+//			return -1;
+//		}
+//		return 0;
+//	}
+//}
 
+class ConsomeString implements Consumer<String> {
 	@Override
-	public int compare(String o1, String o2) {
-		if(o1.length() > o2.length()) {
-			return 1;
-		}
-		if(o1.length() < o2.length()) {
-			return -1;
-		}
-		return 0;
-	}
-	
+	public void accept(String t) {
+		System.out.println(t);		
+	}	
 }
