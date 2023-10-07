2b746a2914440263d9a35db53685566e77837e0a
Pedro Henrique <pedrotxc@gmail.com>
2023-10-07 16:31:20 -0300

    Commit pattern strategy

---
+++ src/strategy/Strategy_4.java
@@ -0,0 +1,28 @@
+package strategy;
+
+import java.math.BigDecimal;
+
+public class Strategy_4 {
+	public static void main(String[] args) {
+		BigDecimal valor = new BigDecimal("10");
+		Compra_4 compra_4 = new Compra_4(valor);
+		
+		compra_4.processarCompra(v -> System.out.println("Pagou no crédito: " + valor));
+	}
+}
+
+class Compra_4{
+	BigDecimal valor;
+	
+	public Compra_4(BigDecimal valor) {
+		this.valor = valor;
+	}
+	
+	void processarCompra(EstrategiaPagamento_4 estrategiaPagamento_4) {
+		estrategiaPagamento_4.pagar(this.valor);
+	}
+}
+
+interface EstrategiaPagamento_4 {
+	void pagar(BigDecimal valor);
+}
