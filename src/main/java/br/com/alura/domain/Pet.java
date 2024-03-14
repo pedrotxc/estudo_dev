7dc33b55de554730cbbcffbb4052afebe1daade2
Pedro Henrique <pedrotxc@gmail.com>
2024-03-13 23:55:56 -0300

    Atualizando Projeto com as separacoes do codigo

---
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -0,0 +1,53 @@
+package br.com.alura.domain;
+
+public class Pet {
+
+    private Long id;
+    private String tipo;
+    private String nome;
+    private String raca;
+    private Integer idade;
+    private String cor;
+    private Float peso;
+
+    public Pet(){
+
+    }
+
+    public Pet(String tipo, String nome, String raca, Integer idade, String cor, Float peso) {
+        this.tipo = tipo;
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+        this.cor = cor;
+        this.peso = peso;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public Float getPeso() {
+        return peso;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+}
